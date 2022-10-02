df11a05f5bdefb67f8ad0fc5842a400d6ab244d4
sonic011gamer <efimgirjeu@gmail.com>
2022-10-02 18:03:48 +0300

    ci-build
    try to implement this in a cleaner way

---
+++ ci-build.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+curdir="$PWD"
+git clone https://github.com/Tianocore/edk2.git -b stable/202011 --depth=1 --recursive ../edk2
+cd ../edk2
+make -C BaseTools/
+. edksetup.sh
+cd ..
+GCC5_ARM_PREFIX=arm-linux-gnueabihf- build -a ARM -p Lumia650Pkg/Lumia650.dsc -t GCC5 -j$(nproc) -s -n 0
+./Lumia650Pkg/build_boot_shim.sh
+./Lumia650Pkg/build_boot_images.sh
+cd $curdir
