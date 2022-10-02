2003c242ff96e82aefe8674aa4f520d8869255f6
sonic011gamer <efimgirjeu@gmail.com>
2022-10-02 18:40:51 +0300

    forgor :punch:

--- build_boot_images.sh
+++ build_boot_images.sh
@@ -2,4 +2,4 @@
 
 cat ./BootShim/BootShim.bin ../edk2/Build/Lumia650-ARM/DEBUG_GCC5/FV/MSM8909_EFI.fd ./ImageResources/dummykernel > ./ImageResources/bootpayload.bin
 gzip -c < ./ImageResources/bootpayload.bin > ./ImageResources/bootpayload.bin.gz
-cat ./ImageResources/bootpayload.bin.gz huawei-y560.dtb > ./ImageResources/uefi.img
+cat ./ImageResources/bootpayload.bin.gz ./ImageResources/huawei-y560.dtb > ./ImageResources/uefi.img
