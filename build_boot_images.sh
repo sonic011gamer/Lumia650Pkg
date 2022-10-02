1bb2db5c31b40558ecf7af006ebea6a4d2d6eb4c
sonic011gamer <efimgirjeu@gmail.com>
2022-10-02 18:22:55 +0300

    Revert the Memory map
    i forgot to revert it from the experimental one

--- build_boot_images.sh
+++ build_boot_images.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-cat ./BootShim/BootShim.bin ../Build/Lumia650-ARM/DEBUG_GCC5/FV/MSM8909_EFI.fd ./ImageResources/dummykernel > ./ImageResources/bootpayload.bin
+cat ./BootShim/BootShim.bin ../edk2/Build/Lumia650-ARM/DEBUG_GCC5/FV/MSM8909_EFI.fd ./ImageResources/dummykernel > ./ImageResources/bootpayload.bin
 
 python3 ./ImageResources/mkbootimg.py \
   --kernel ./ImageResources/bootpayload.bin \
