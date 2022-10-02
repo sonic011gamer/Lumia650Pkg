b2c7a56147ac98da85f367aa1b4e0c7a141f8bfe
sonic011gamer <efimgirjeu@gmail.com>
2022-10-02 18:35:22 +0300

    Update bootimg script
    hope it detects dtb now

--- build_boot_images.sh
+++ build_boot_images.sh
@@ -1,15 +1,5 @@
 #!/bin/bash
 
 cat ./BootShim/BootShim.bin ../edk2/Build/Lumia650-ARM/DEBUG_GCC5/FV/MSM8909_EFI.fd ./ImageResources/dummykernel > ./ImageResources/bootpayload.bin
-
-python3 ./ImageResources/mkbootimg.py \
-  --kernel ./ImageResources/bootpayload.bin \
-  --ramdisk ./ImageResources/dummykernel \
-  -o ./ImageResources/uefi.img \
-  --pagesize 2048 \
-  --header_version 0 \
-  --cmdline "" \
-  --dtb ./ImageResources/huawei-y560.dtb \
-  --base 0x0 \
-  --os_version 7.1.1 \
-  --os_patch_level 2022-10-02 \
+gzip -c < ./ImageResources/bootpayload.bin > ./ImageResources/bootpayload.bin.gz
+cat ./ImageResources/bootpayload.bin.gz huawei-y560.dtb > ./ImageResources/uefi.img
