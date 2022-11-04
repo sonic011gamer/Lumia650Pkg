332d6bf1ff4e2f103a2c829b73630d89d368ed47
sonic011gamer <58566108+sonic011gamer@users.noreply.github.com>
2022-11-04 21:45:11 +0200

    Attempt to make it bootable

--- build_boot_images.sh
+++ build_boot_images.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
 
-cat ../edk2/Build/Lumia650-ARM/DEBUG_GCC5/FV/MSM8909_EFI.fd > ./ImageResources/bootpayload.bin
-gzip -c < ./ImageResources/bootpayload.bin > ./ImageResources/bootpayload.bin.gz
-cat ./ImageResources/bootpayload.bin.gz ./ImageResources/huawei-y560.dtb >> ./ImageResources/Image.gz-dtb
-mkbootimg --kernel ./ImageResources/Image.gz-dtb --ramdisk ./ImageResources/dummykernel --base 0x80000000 --pagesize 2048 -o ./ImageResources/uefi.img
+gzip -c < ../edk2/Build/Lumia650-ARM/DEBUG_GCC5/FV/MSM8909_EFI.fd >./ImageResources/uefi.img
+cat ./ImageResources/huawei-y560.dtb >>./ImageResources/uefi.img
