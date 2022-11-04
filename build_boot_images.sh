834a56baf1346399273950a741756e2efb249729
sonic011gamer <58566108+sonic011gamer@users.noreply.github.com>
2022-11-04 22:13:33 +0200

    aughh

--- build_boot_images.sh
+++ build_boot_images.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-gzip -c < ../edk2/Build/Lumia650-ARM/DEBUG_GCC5/FV/MSM8909_EFI.fd >./ImageResources/uefi.img
-cat ./ImageResources/huawei-y560.dtb >>./ImageResources/uefi.img
+gzip -c < ../edk2/Build/Lumia650-ARM/DEBUG_GCC5/FV/MSM8909_EFI.fd >MSM8909_UEFI.fd.gz
+skales-mkbootimg --kernel MSM8909_UEFI.fd.gz --dt ImageResources/huawei-y560.dtb --ramdisk ImageResources/empty --base 0x80000000 --pagesize 2048 --cmdline "androidboot.hardware=qcom msm_rtb.filter=0x3F ehci-hcd.park=3 androidboot.bootdevice=7824900.sdhci lpm_levels.sleep_disabled=1 earlyprintk androidboot.selinux=permissive" --output ./ImageResources/uefi.img
