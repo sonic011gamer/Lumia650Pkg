42ff6f10dcc860c9c85e26b7a863ed04cb913093
sonic011gamer <58566108+sonic011gamer@users.noreply.github.com>
2023-03-09 00:14:15 -0800

    something idk

--- build_boot_shim.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-cd BootShim
-rm BootShim.bin
-rm BootShim.elf
-
-make UEFI_BASE=0x80200000 UEFI_SIZE=0x00100000
-cd ..
--- ci-build.sh
+++ ci-build.sh
@@ -2,11 +2,12 @@
 curdir="$PWD"
 git clone https://github.com/Tianocore/edk2.git -b stable/202011 --depth=1 --recursive ../edk2
 export PACKAGES_PATH=$PWD/../edk2:$PWD/../:$PWD
+export WORKSPACE=$PWD/workspace
 cd ../edk2
 make -C BaseTools/
 . edksetup.sh
 cd ..
 GCC5_ARM_PREFIX=arm-linux-gnueabihf- build -a ARM -p ../Lumia650Pkg/Lumia650.dsc -t GCC5 -j$(nproc) -s -n 0
 cd $curdir
-./build_boot_images.sh
-
+gzip -c < workspace/Build/MSM8909/DEBUG_GCC5/FV/MSM8909_UEFI.fd >MSM8909_UEFI.fd.gz
+skales-mkbootimg --kernel MSM8909_UEFI.fd.gz --dt huawei-y560.dtb --ramdisk workspace/dummy --base 0x80000000 --pagesize 2048 --cmdline "androidboot.hardware=qcom msm_rtb.filter=0x3F ehci-hcd.park=3 androidboot.bootdevice=7824900.sdhci lpm_levels.sleep_disabled=1 earlyprintk androidboot.selinux=permissive" --output uefi.img
